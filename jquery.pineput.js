00e8174b7b2e7472ec2bf47c25c47ed4020a404c
Nikola Stamatovic <stamatmail@gmail.com>
2021-03-15 14:37:24 +0100

    added select element support

--- jquery.pineput.js
+++ jquery.pineput.js
@@ -1,57 +1,57 @@
 (function($) {
-	$.fn.pineput = function() {
-		var $this = $(this);
-		for (var i = 0; i < $this.length; i++) {
-			var $input_fields = $($this[i]).find('.input-field input:not([type="submit"]), .input-field textarea');
+  $.fn.pineput = function() {
+    var $this = $(this);
+    for (var i = 0; i < $this.length; i++) {
+      var $input_fields = $($this[i]).find('.input-field input:not([type="submit"]), .input-field textarea, .input-field select');
 
-			// TODO: custom selector for input fields property
-			// TODO: other form items like select, checkbox, radio button etc...
+      // TODO: custom selector for input fields property
+      // TODO: other form items like checkbox, radio button etc...
 
-	        $input_fields.on('focus', function(e){
-	            var $field = $(this).closest('.input-field');
-	            $field.addClass('focus');
-	        }).on('blur', function(e){
-	            var $this = $(this);
-	            var $field = $this.closest('.input-field');
-	            $field.removeClass('focus');
+      $input_fields.on('focus', function(e){
+        var $field = $(this).closest('.input-field');
+        $field.addClass('focus');
+      }).on('blur', function(e){
+        var $this = $(this);
+        var $field = $this.closest('.input-field');
+        $field.removeClass('focus');
 
-	            var val = $this.val().trim();
+        var val = $this.val().trim();
 
-	            if (val === '') {
-	                $field.removeClass('active');
-	            } else {
-	                $field.addClass('active');
-	            }
-	        }).on('change', function(e){
-	            var $this = $(this);
-	            var $field = $this.closest('.input-field');
-	            var val = $this.val().trim();
+        if (val === '') {
+          $field.removeClass('active');
+        } else {
+          $field.addClass('active');
+        }
+      }).on('change', function(e){
+        var $this = $(this);
+        var $field = $this.closest('.input-field');
+        var val = $this.val().trim();
 
-	            if (val === '') {
-	                $field.removeClass('active');
-	            } else {
-	                $field.addClass('active');
-	            }
-	        });
+        if (val === '') {
+          $field.removeClass('active');
 
-	       	for (var j = 0; j < $input_fields.length; j++) {
-	           	var $input_field = $($input_fields[j]);
+          if ($this.is('select')) {
+            $this.trigger('blur');
+          }
+        } else {
+          $field.addClass('active');
+        }
+      });
 
-	           	var $field = $input_field.closest('.input-field');
-	           	var val = $input_field.val().trim();
+      for (var j = 0; j < $input_fields.length; j++) {
+        var $input_field = $($input_fields[j]);
 
-	           	if (val === '') {
-	               	$field.removeClass('active');
-	           	} else {
-	               	$field.addClass('active');
-	           	}
-	       	}
+        var $field = $input_field.closest('.input-field');
+        var val = $input_field.val().trim();
 
-	       	$this.find('label').on('click', function(){
-	           	$(this).closest('.input-field').find('input, textarea').trigger('focus');
-	       	});
+        if (val === '') {
+          $field.removeClass('active');
+        } else {
+          $field.addClass('active');
+        }
+      }
 
-	       	$this.find('textarea').closest('.input-field').addClass('textarea');
-		}
+      $this.find('textarea').closest('.input-field').addClass('textarea');
     }
+  }
 })(jQuery);
